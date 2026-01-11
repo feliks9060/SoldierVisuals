b38ee142baace59799aadd1afd93697549fe4790
feliks9060 <kdoros75@gmail.com>
2026-01-11 18:00:27 +0200

    Add files via upload

---
+++ src/main/java/com/example/soldiervisuals/SoldierVisuals.java
@@ -0,0 +1,10 @@
+package com.example.soldiervisuals;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SoldierVisuals implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("SoldierVisuals loaded!");
+    }
+}
